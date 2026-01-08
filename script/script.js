cb1b4384edd60fc1658c8ce8104247be2e2a84f9
phlopa <203694213+phlopa@users.noreply.github.com>
2026-01-08 21:41:11 +0100

    PR4_3

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const formAnswers = document.querySelector("#formAnswers");
     const prevButton = document.querySelector("#prev");
     const nextButton = document.querySelector("#next");
-
+    const sendButton = document.querySelector("#send");
 
     
 
@@ -99,14 +99,16 @@ document.addEventListener("DOMContentLoaded",()=>{
     })
 
     const playTest = ()=> {
+        
+        const finalAnswers=[];
 
         let numberQuestion=0;
-
+/*
         const renderButtons= ()=>{
             prevButton.style.display = numberQuestion === 0 ? 'none' : 'inline-block';
             nextButton.style.display = numberQuestion === questions.length - 1 ? 'none' : 'inline-block';
         }
-
+*/
         const renderAnswers= (index)=>{
             questions[index].answers.forEach((answer)=>{
                 const answerItem = document.createElement('div');
@@ -115,7 +117,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                  answerItem.innerHTML=
                 `
                 
-                    <input type="radio" id="${answer.title}" name="answer" class="d-none">
+                    <input type="${questions[index].type}" id="${answer.title}" name="answer" value=${answer.title} class="d-none">
                     <label for="${answer.title}" class="d-flex flex-column justify-content-between">
                         <img class="answerImg" src="${answer.url}" alt="burger">
                         <span>${answer.title}</span>
@@ -128,11 +130,108 @@ document.addEventListener("DOMContentLoaded",()=>{
 
         const renderQuestions=(indexQuestion)=>{
             formAnswers.innerHTML='';
-            questionTitle.textContent=`${questions[indexQuestion].question}`;
-            renderAnswers(indexQuestion);
-            renderButtons();
+            switch (true) {
+                case numberQuestion === 0:
+                    questionTitle.textContent = questions[indexQuestion].question;
+                    formAnswers.innerHTML = '';
+                    renderAnswers(indexQuestion);
+
+                    prevButton.classList.add('d-none');
+                    nextButton.classList.remove('d-none');
+                    sendButton.classList.add('d-none');
+                    break;
+
+                case numberQuestion > 0 && numberQuestion <= questions.length - 1:
+                    questionTitle.textContent = questions[indexQuestion].question;
+                    formAnswers.innerHTML = '';
+                    renderAnswers(indexQuestion);
+
+                    prevButton.classList.remove('d-none');
+                    nextButton.classList.remove('d-none');
+                    sendButton.classList.add('d-none');
+                    break;
+
+                case numberQuestion === questions.length:
+                    questionTitle.textContent = '';
+                    formAnswers.innerHTML = `
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text">Ведіть номер телефону</span>
+                        <input type="text" id="NumberPhone" class="form-control"
+                            placeholder="Ведіть номер телефону..">
+                    </div>
+                    `;
+
+                    prevButton.classList.add('d-none');
+                    nextButton.classList.add('d-none');
+                    sendButton.classList.remove('d-none');
+                    break;
+
+                case numberQuestion === questions.length + 1:
+                    formAnswers.innerHTML = `
+                    Дякуємо за замовлення! Менеджер вам зателефонує.
+                    `;
+
+                    setTimeout(() => {
+                    modalBlock.classList.remove('d-block');
+                    }, 2000);
+                    break;
+            }
+            /*
+            if(numberQuestion>=0&&numberQuestion<=questions.length-1){
+                questionTitle.textContent=`${questions[indexQuestion].question}`;
+                renderAnswers(indexQuestion);
+
+                nextButton.classList.remove('d-none');
+                prevButton.classList.remove('d-none');
+            }
+            if(numberQuestion===0){
+                prevButton.classList.add('d-none');
+                sendButton.classList.add('d-none');
+            }
+            if(numberQuestion===questions.length){
+                questionTitle.textContent='';
+                nextButton.classList.add('d-none');
+                prevButton.classList.add('d-none');
+                sendButton.classList.remove('d-none');
+
+                formAnswers.innerHTML=
+                `
+                <div class="input-group flex-nowrap">
+                    <span class="input-group-text" id="NumberPhone">Ведіть номер телефону</span>
+                    <input type="text" id="NumberPhone" class="form-control" placeholder="Ведіть номер телефону.." aria-label="Username" aria-describedby="addon-wrapping">
+                </div>
+                `
+            }
+            if(numberQuestion===questions.length+1){
+                formAnswers.innerHTML=
+                `
+                Дякуємо за замовлення! Менеджер вам зателефонує.
+                `
+                setTimeout(()=>{
+                    modalBlock.classList.remove('d-block');
+                },2000)
+            }*/
+            
+            //renderButtons();
         }
         renderQuestions(numberQuestion);
+
+        const checkAnswers=()=>{
+            const obj = {};
+            const inputs = [...formAnswers.elements].filter((input)=>input.checked||input.id==="NumberPhone");
+            
+            inputs.forEach((input, index)=>{
+                if(numberQuestion>=0&&numberQuestion<=questions.length-1){
+                    obj[`${index}_${questions[numberQuestion].question}`]=input.value;
+                }
+                if(numberQuestion===questions.length){
+                    obj['Number Phone']=input.value;
+                }
+            })
+            finalAnswers.push(obj);
+            console.log(finalAnswers);
+            
+        }
 
         prevButton.onclick=()=>{
             numberQuestion--;
@@ -140,8 +239,16 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
 
         nextButton.onclick=()=>{
+            checkAnswers();
             numberQuestion++;
             renderQuestions(numberQuestion);
+        }
+
+        sendButton.onclick=()=>{
+            checkAnswers();
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+            console.log(finalAnswers);
         }
     }
 })
